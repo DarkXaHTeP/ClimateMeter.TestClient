615c471bab134c0316be9a2a49ce2e743d1a0ed0
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-29 03:13:29 +0200

    Used commandline to build and run app

--- Program.cs
+++ Program.cs
@@ -1,32 +1,21 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.SignalR.Client;
+﻿using DarkXaHTeP.CommandLine;
+using Microsoft.Extensions.Logging;
 
 namespace ClimateMeter.TestClient
 {
-    class Program
+    static class Program
     {
-        static async Task Main(string[] args)
+        static int Main(string[] args)
         {
-            var connection = new HubConnectionBuilder()
-                .WithUrl("http://localhost:5000/socket/device")
-                .WithJsonProtocol()
-                .WithConsoleLogger()
+            ICommandLineHost host = new CommandLineHostBuilder()
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                })
+                .UseStartup<Startup>()
                 .Build();
 
-            connection.On<Guid>("DeviceRegistered", async id =>
-            {
-                Console.WriteLine($"Registered with Id: {id}");
-                await connection.InvokeAsync("AddSensorReading", id, 26.0, 43.0);
-            });
-
-            await connection.StartAsync();
-
-            await connection.InvokeAsync("RegisterDevice", "Test Device", "Test description");
-
-            Console.ReadKey();
-
-            await connection.DisposeAsync();
+            return host.Run(args);
         }
     }
 }
