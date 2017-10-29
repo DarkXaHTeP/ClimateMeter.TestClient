f99a715fcc41bda95a90ae85836ff005bbc43082
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-29 03:12:56 +0200

    Added startup with commands mappings

---
+++ Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using DarkXaHTeP.CommandLine;
+using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ClimateMeter.TestClient
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder builder, TestSignalRClient client, ILogger<Startup> log)
+        {
+            builder.OnExecute(async () =>
+            {
+                var deviceId = await client.StartDevice();
+                await Task.Delay(2000);
+                await client.AddSensorReading(deviceId, 24.5m, 68.3m);
+                await client.Disconnect();
+                return 0;
+            });
+
+            builder.Command("interval", command =>
+            {
+                var everyOption = command.Option("--every", "Interval in seconds", CommandOptionType.SingleValue);
+                var iterationsOption = command.Option(
+                    "--iterations", "Amount of iterations to run",
+                    CommandOptionType.SingleValue);
+
+                command.OnExecute(async () =>
+                {
+                    int every = everyOption.HasValue() ? Convert.ToInt32(everyOption.Value()) : 10;
+                    int iterations = iterationsOption.HasValue() ? Convert.ToInt32(iterationsOption.Value()) : Int32.MaxValue;
+
+                    Guid deviceId = await client.StartDevice();
+
+                    for (int i = 0; i < iterations; i++)
+                    {
+                        await client.AddSensorReading(deviceId, 23m, 45m);
+                        await Task.Delay(every * 1000);
+                    }
+
+                    await client.Disconnect();
+                    
+                    return 0;
+                });
+            });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<TestSignalRClient>();
+        }
+    }
+}
