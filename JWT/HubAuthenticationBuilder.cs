a4a33a26aeaea0264e5ef0b03bc962627e1795e9
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-31 20:05:16 +0200

    Added HubAuthenticationBuilder implementation

---
+++ JWT/HubAuthenticationBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ClimateMeter.TestClient.JWT
+{
+    public class HubAuthenticationBuilder: IHubAuthenticationBuilder
+    {
+        private readonly Func<string> _accessTokenProvider;
+        private readonly IHubConnectionBuilder _connectionBuilder;
+        private string _tokenQueryStringParameterName = "JWT_TOKEN";
+
+        public HubAuthenticationBuilder(Func<string> accessTokenProvider, IHubConnectionBuilder connectionBuilder)
+        {
+            _accessTokenProvider = accessTokenProvider;
+            _connectionBuilder = connectionBuilder;
+        }
+
+        public IHubAuthenticationBuilder WithTokenQueryStringParameterName(string queryStringParameterName)
+        {
+            if (String.IsNullOrEmpty(queryStringParameterName))
+            {
+                throw new ArgumentNullException(nameof(queryStringParameterName));
+            }
+
+            _tokenQueryStringParameterName = queryStringParameterName;
+
+            return this;
+        }
+
+        public IHubConnectionBuilder WithUrl(string url)
+        {
+            return WithUrl(new Uri(url));
+        }
+
+        public IHubConnectionBuilder WithUrl(Uri url)
+        {
+            var uriBuilder = new UriBuilder(url);
+            
+            var queryString = HttpUtility.ParseQueryString(uriBuilder.Query);
+            if (queryString[_tokenQueryStringParameterName] != null)
+            {
+                throw new ArgumentException($"Url should not contain Query String parameter named {_tokenQueryStringParameterName}", nameof(url));
+            }
+            
+            var token = _accessTokenProvider();
+            queryString[_tokenQueryStringParameterName] = token;
+
+            uriBuilder.Query = queryString.ToString();
+
+            return _connectionBuilder
+                .WithMessageHandler(new AuthorizationHttpMessageHandler(_accessTokenProvider))
+                .WithUrl(uriBuilder.Uri);
+        }
+    }
+}
