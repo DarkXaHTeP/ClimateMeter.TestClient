f9e39f3db397afcac7f7d7e46d3c84c65fcf9e0a
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-31 20:04:48 +0200

    Added AuthenticationHttpMessageHandler

---
+++ JWT/AuthorizationHttpMessageHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClimateMeter.TestClient.JWT
+{
+    public class AuthorizationHttpMessageHandler: DelegatingHandler
+    {
+            private Func<string> _accessTokenProvider { get; set; }
+        
+            public AuthorizationHttpMessageHandler(Func<string> accessTokenProvider)
+            {
+                _accessTokenProvider = accessTokenProvider;
+                InnerHandler = new HttpClientHandler();
+            }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                var token = _accessTokenProvider();
+                request.Headers.Add("Authorization", $"Bearer {token}");
+            
+                return base.SendAsync(request, cancellationToken);
+            }
+        }
+    }
