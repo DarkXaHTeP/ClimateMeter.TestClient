25551ebf77b2251e263db2178a33c4828a7457bc
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-31 20:05:33 +0200

    Added connection builder extensions for authentication

---
+++ JWT/HubConnectionBuilderAuthorizationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ClimateMeter.TestClient.JWT
+{
+    public static class HubConnectionBuilderAuthorizationExtensions
+    {
+        public static IHubAuthenticationBuilder WithJWTAuthentication(this IHubConnectionBuilder builder, Func<string> accessTokenProvider)
+        {
+            return new HubAuthenticationBuilder(accessTokenProvider, builder);
+        }
+        
+        public static IHubAuthenticationBuilder WithJWTAuthentication(this IHubConnectionBuilder builder, Func<Task<string>> accessTokenProvider)
+        {
+            return new HubAuthenticationBuilder(() => accessTokenProvider().GetAwaiter().GetResult(), builder);
+        }
+    }
+}
