5474de12e3701cdac24d04e20d67bce542505aea
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-31 20:04:00 +0200

    Added interface for hub authentication builder

---
+++ JWT/IHubAuthenticationBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ClimateMeter.TestClient.JWT
+{
+    public interface IHubAuthenticationBuilder
+    {
+        IHubAuthenticationBuilder WithTokenQueryStringParameterName(string queryStringParameterName);
+        IHubConnectionBuilder WithUrl(string url);
+        IHubConnectionBuilder WithUrl(Uri url);
+    }
+}
